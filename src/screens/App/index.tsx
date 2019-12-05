5240b4c3dc6f91f2b48b92119e7dfe823860a71c
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-12-05 15:23:34 +0330

    implements circular navigation for Welcome and FameList screen

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Options } from 'react-native-navigation';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import { setRootAppStack } from '../../navigation';
 import Welcome from './Welcome';
 import Slide from './Slide';
 import FameList from './FameList';
+import { useBackHandler } from '../../utils/hooks';
 
 const slides = [Welcome, FameList];
 
-function AppScreen(): JSX.Element {
+function AppScreen({ componentId }): JSX.Element {
+    let slider = useRef();
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [backRequested, setBackRequested] = useBackHandler(componentId);
+    useEffect(() => {
+        if (backRequested && slider) {
+            const index = Math.abs(currentIndex + 1 - slides.length);
+            slider.goToSlide(index);
+            setCurrentIndex(index);
+            setBackRequested(false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [backRequested]);
     return (
         <AppIntroSlider
+            ref={(s): void => {
+                slider = s;
+            }}
             slides={slides}
             renderItem={Slide}
+            hidePagination={true}
+            onSlideChange={setCurrentIndex}
             keyExtractor={(item, index): string => index.toString()}
             onDone={setRootAppStack}
         />
